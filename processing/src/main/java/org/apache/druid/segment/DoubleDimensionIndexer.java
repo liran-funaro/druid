5e756c37aeaea1d3af5272face317c83ac8c623b
Liran Funaro <fonaro@gmail.com>
2020-05-18 16:10:12 +0300

    Introduce OakIncrementalIndex

--- processing/src/main/java/org/apache/druid/segment/DoubleDimensionIndexer.java
+++ processing/src/main/java/org/apache/druid/segment/DoubleDimensionIndexer.java
@@ -112,20 +112,19 @@ public class DoubleDimensionIndexer implements DimensionIndexer<Double, Double, 
       @Override
       public boolean isNull()
       {
-        final Object[] dims = currEntry.get().getDims();
-        return dimIndex >= dims.length || dims[dimIndex] == null;
+        return currEntry.get().isDimNull(dimIndex);
       }
 
       @Override
       public double getDouble()
       {
-        final Object[] dims = currEntry.get().getDims();
+        final Object dim = currEntry.get().getDim(dimIndex);
 
-        if (dimIndex >= dims.length || dims[dimIndex] == null) {
+        if (dim == null) {
           assert NullHandling.replaceWithDefault();
           return 0.0;
         }
-        return (Double) dims[dimIndex];
+        return (Double) dim;
       }
 
       @SuppressWarnings("deprecation")
@@ -133,12 +132,12 @@ public class DoubleDimensionIndexer implements DimensionIndexer<Double, Double, 
       @Override
       public Double getObject()
       {
-        final Object[] dims = currEntry.get().getDims();
+        final Object dim = currEntry.get().getDim(dimIndex);
 
-        if (dimIndex >= dims.length || dims[dimIndex] == null) {
+        if (dim == null) {
           return NullHandling.defaultDoubleValue();
         }
-        return (Double) dims[dimIndex];
+        return (Double) dim;
       }
 
       @Override
